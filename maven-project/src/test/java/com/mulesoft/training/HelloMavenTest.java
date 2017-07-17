00ada8a113bca36d188bf753ee30c9ba971df283
Gopal Erusu <Gopal Erusu>
2017-07-17 15:58:07 -0500

    Added Dynamic Ports for JUnit and a new test case to check Content-Type

--- maven-project/src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ maven-project/src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,13 +2,20 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
+	
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println("\n\nIn Test Case-1: Dynamic Http port ------> " + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
@@ -19,4 +26,11 @@ public class HelloMavenTest extends FunctionalTestCase {
     	return files;
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      System.out.println("\n\nIn Test Case-2: Dynamic Http port ------> " + dynamicPort.getNumber() + "\n\n");
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
 }
